2cde470538a6c6d3dd5276642d53a3a0d9a50135
Vanessa Dohnt <vanessa.e.dohnt@gmail.com>
2017-05-31 09:57:06 +1000

    add intro html and js

---
+++ week01/01-javascript/intro/main.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+
+if (5 > 4) {
+  console.log('math is easy');
+}
+
+var wdi12 = true; // global variable
+
+// scope
+
+if (wdi12) {
+  console.log('wdi12 rocks!');
+}
