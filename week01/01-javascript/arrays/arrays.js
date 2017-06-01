b48795d2c883f5e7db730ea05950b817d9e3b447
Vanessa.Dohnt <vanessa.e.dohnt@gmail.com>
2017-06-01 16:21:40 +1000

    overdue work

---
+++ week01/01-javascript/arrays/arrays.js
@@ -0,0 +1,9 @@
+// var pm = ['turnbull', 'rudd', 'gilliard', 'hawk']
+//
+// for (var i = 0; i < pm.length; i++) {
+//   console.log('my first choice is' + [2]);
+// }
+//
+
+// languages ['python','perl','ruby','c++']
+// languages.push('ruby')
---
+++ week01/01-javascript/objects/objects.js
@@ -0,0 +1,20 @@
+var student = {
+  name: 'Kit',
+  paid: true,
+  bank balance: 10000000000
+}
+
+var languages = {
+  0: 'ruby',
+  1: 'python',
+  2: 'php'
+}
+
+var wdi12 = {
+  'class name': bitsPlease,
+  students: ['a','b','c'],
+  campus: 'Melbourne'
+}
+
+var months = ['Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sep','Oct','Nov','Dec'];
+var energy = months.join(separator);
